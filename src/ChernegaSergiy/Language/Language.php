bb7a78bfe2831d1b8135033273054baff5a71755
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-06 20:37:46 +0300

    refactor(language): Make Language class immutable

--- src/ChernegaSergiy/Language/Language.php
+++ src/ChernegaSergiy/Language/Language.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace ChernegaSergiy\Language;
 
-class Language {
+final class Language {
 
-    private string $locale;
-    private array $translations;
-
-    public function __construct(string $locale, array $translations) {
-        $this->locale = $locale;
-        $this->translations = $translations;
-    }
+    public function __construct(
+        private readonly string $locale,
+        private readonly array $translations
+    ) {}
 
     public function getLocale(): string {
         return $this->locale;
